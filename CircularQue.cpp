805aabe95ecbb19df9ff9c1fef30305406230792
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2025-06-12 19:04:31 +0700

    Add switch-case control for user menu options

--- CircularQue.cpp
+++ CircularQue.cpp
@@ -130,4 +130,35 @@ int main()
             cout << endl;
         }
 
+        switch (ch)
+        {
+            case'1':
+            {
+                q.insert()
+                break;
+            }
+            case'2':
+            {
+                q.remove();
+                break;
+            }
+            case '3':
+            {
+                q.display();
+                break;
+            }
+            case '4':
+            {
+                return 0;
+            }
+            default:
+            {
+                cout << "Invalid option!!" <<endl;
+                break;
+            }
         
+        }
+ 
+   
+   
+         
